0c754771066441c53caed2ff898b1391f664206b
jackeryjam <jackeryjam@gmail.com>
2017-11-09 14:03:37 +0800

    django

--- eightqueen/check.py
+++ eightqueen/check.py
@@ -1,8 +1,37 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 import json
 
+def bitCalc(chessBoard):
+    flag = True
+    n = len(chessBoard)
+    r = 0
+    l = 0
+    c = 0
+    for i in range(len(chessBoard)):
+        if flag == False:
+            break
+        row = 0
+        for j in range(len(chessBoard[i])):
+            if chessBoard[i][j] == False:
+                continue
+            tmp = 1 << j
+            if (tmp&l) or (tmp&r) or (tmp&c) or (tmp&row):
+                flag = False
+                break
+            else:
+                l = (l|tmp)
+                r = (r|tmp)
+                c = (c|tmp)
+                row = (1<<n) - 1
+        l = l << 1
+        r = r >> 1
+    return flag
+
 def index(request): 
-    res = {"code":1,"msg":"","data":{"c_cnt":29,"o_cnt":0,"p_cnt":6,"pri_cnt":0}}
-    if 'chessboard' in request.POST:
-        print(request.POST['chessboard'])
-    return HttpResponse(json.dumps(res))
+    result = {
+        'isLegal':False
+    }
+    if 'chessBoard' in request.POST:
+        chessBoard = json.loads(request.POST['chessBoard'])
+        result['isLegal'] = bitCalc(chessBoard)
+    return JsonResponse(result)
